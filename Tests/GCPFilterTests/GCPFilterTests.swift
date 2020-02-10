4d967b66329ebc9eceaeef5ce1b1f7a088768f1a
andyksaw <andy-saw@hotmail.com>
2020-02-11 02:08:13 +0900

    Add simple test

--- Tests/GCPFilterTests/GCPFilterTests.swift
+++ Tests/GCPFilterTests/GCPFilterTests.swift
@@ -2,14 +2,23 @@ import XCTest
 @testable import GCPFilter
 
 final class GCPFilterTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-//        XCTAssertEqual(GCPFilter().text, "Hello, World!")
+    enum TestField: String {
+        case displayName = "display_name"
+        case description = "description"
+        case userLabels = "user_labels"
+    }
+
+    func testAndToken() {
+        let query = BuildQuery(for: TestField.self) {
+            And {
+                Expression(.displayName, .equal, rawValue: "test_name")
+                Expression(.description, .equal, rawValue: "test_description")
+            }
+        }
+        XCTAssertEqual(query, "(display_name='test_name' AND description='test_description')")
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testAndToken", testAndToken),
     ]
 }
